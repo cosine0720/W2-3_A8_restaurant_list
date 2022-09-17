1c45c728e73a17efb4a97f4e215a03fc6be4f820
cosine0720 <cosine0720@gmail.com>
2022-09-17 15:06:31 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const restaurantList = require('./restaurant.json')
 
 const port = 3000
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
