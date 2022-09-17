c3bffe381340a5bab1c8ec54eb35ed0c5313f874
cosine0720 <cosine0720@gmail.com>
2022-09-17 12:28:18 +0800

    feat: add static files

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// setting static files
+app.use(express.static('public')) // 使用css靜態檔案！！！
+
 app.get('/',(req, res) => {
   res.render('index')
 })
