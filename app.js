45a251bca9924e449f2583bce5debef499aaa6a7
cosine0720 <cosine0720@gmail.com>
2022-09-18 22:54:11 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const Restaurant = require('./models/restaurant')
+const routes = require('./routes')
 
 const app = express()
 const port = 3000
@@ -25,83 +26,7 @@ app.set('view engine', 'hbs')
 app.use(express.static('public')) // 使用css靜態檔案！！！
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
-// 首頁
-app.get('/', (req, res) => {
-  Restaurant.find({})
-    .lean()
-    .sort({ _id: 'asc' })
-    .then(restaurantsData => res.render("index", { restaurantsData }))
-    .catch(err => console.log(err))
-})
-
-// 搜尋功能
-app.get('/search', (req, res) => {
-  if (!req.query.keywords) {
-    res.redirect("/")
-  }
-
-  // 搜尋餐廳名稱關鍵字 || 餐廳種類關鍵字
-  const keywords = req.query.keywords
-  const keyword = req.query.keywords.trim().toLowerCase()
-
-  Restaurant.find({})
-    .lean()
-    .then(restaurantsData => {
-      const filterRestaurantsData = restaurantsData.filter(
-        data =>
-          data.name.toLowerCase().includes(keyword) ||
-          data.category.includes(keyword)
-      )
-      res.render("index", { restaurantsData: filterRestaurantsData, keywords })
-    })
-    .catch(err => console.log(err))
-})
-
-
-// 新增餐廳頁面
-app.get("/restaurants/new", (req, res) => {
-  res.render("new")
-})
-
-app.post('/restaurants', (req, res) => {
-  Restaurant.create(req.body)
-    .then(() => res.redirect("/"))
-    .catch(err => console.log(err))
-})
-
-// 瀏覽餐廳詳細資料
-app.get("/restaurants/:restaurantId", (req, res) => {
-  const { restaurantId } = req.params
-  Restaurant.findById(restaurantId)
-    .lean()
-    .then(restaurantData => res.render("show", { restaurantData }))
-    .catch(err => console.log(err))
-})
-
-// 編輯餐廳頁面
-app.get("/restaurants/:restaurantId/edit", (req, res) => {
-  const { restaurantId } = req.params
-  Restaurant.findById(restaurantId)
-    .lean()
-    .then(restaurantData => res.render("edit", { restaurantData }))
-    .catch(err => console.log(err))
-})
-
-app.put('/restaurants/:restaurantId', (req, res) => {
-  const { restaurantId } = req.params
-  Restaurant.findByIdAndUpdate(restaurantId, req.body)
-    .then(() => res.redirect(`/restaurants/${restaurantId}`))
-    .catch(err => console.log(err))
-})
-
-// 刪除特定餐廳
-app.delete('/restaurants/:restaurantId', (req, res) => {
-  const { restaurantId } = req.params
-  return Restaurant.findByIdAndDelete(restaurantId)
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 
 app.listen(port,() => {
